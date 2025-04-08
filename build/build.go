6435d5386a82a3f0c4b9dc2c6e6b09e32eac583f
Tobias Hort <mail@tobiashort.com>
2025-04-08 07:59:41 +0200

    build - no make

---
+++ build/build.go
@@ -0,0 +1,18 @@
+//go:build ignore
+
+package main
+
+import (
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("go", "build", "-o", "build/url-query-decode")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		os.Exit(1)
+	}
+}
